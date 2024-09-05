1bf003a47773315372e61b23565d89b7c8909087
Bruno1571 <brunoconde29@gmail.com>
2024-09-05 20:22:25 -0300

    cree la vista para crear mensajes

--- mensajes/views.py
+++ mensajes/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Mensaje
 
-# Create your views here.
+def crear_mensaje(request):
+    if request.method == 'POST':
+        texto = request.POST.get('texto')
+        remitente = request.POST.get('remitente')
+        destinatario = request.POST.get('destinatario')
+
+        if texto and remitente and destinatario:
+            Mensaje.objects.create(
+                texto=texto,
+                remitente=remitente,
+                destinatario=destinatario
+            )
+            return redirect('crear_mensaje')  # Redirige a la página principal
+        else:
+            error = "Todos los campos son requeridos."
+            return render(request, 'crear_mensaje.html', {'error': error})
+
+    return render(request, 'crear_mensaje.html')
