11fece7cadc1676c32a9a8a7839fbf8fc628be48
Bruno1571 <brunoconde29@gmail.com>
2024-09-05 14:16:10 -0300

    cree el modelo

--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Mensaje(models.Model):
+    texto = models.TextField()
+    remitente = models.CharField(max_length=100)
+    destinatario = models.CharField(max_length=100)
+    fecha_envio = models.DateTimeField(auto_now_add=True)
