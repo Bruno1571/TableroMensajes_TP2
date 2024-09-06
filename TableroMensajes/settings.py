0c9f87820f43950a7d2487ac91b3925901b3eb0a
Bruno1571 <brunoconde29@gmail.com>
2024-09-06 19:10:22 -0300

    le agregue estilo al proyecto

--- TableroMensajes/settings.py
+++ TableroMensajes/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Esta línea se usa para encontrar archivos estáticos en el directorio 'static' de tu proyecto
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
